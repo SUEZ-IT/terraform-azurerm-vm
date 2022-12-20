20f06112fefb05ab1c03af6e42bc4a9462cdaec3
Cloud Bundle Build Service <azure@devops.com>
2022-12-20 15:07:04 +0000

    preview version

--- 12_insights.tf
+++ 12_insights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_machine_extension" "dependencyagent" {
-  count                      = var.os.type != "Ubuntu" && var.os.version != "2204" ? 1 : 0
+  count                      = local.osfactory_image_name[0] == "UbuntuServer2204" ? 0 : 1
   name                       = "DependencyAgent"
   virtual_machine_id         = var.os.type == "Windows" ? azurerm_windows_virtual_machine.virtual_machine[0].id : azurerm_linux_virtual_machine.virtual_machine[0].id
   publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
